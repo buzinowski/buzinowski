f7b8e3bbba5a9318cf727e38cff5eb9ca72d763a
buzinowski <s.g.buzin@gmail.com>
2017-04-04 18:36:55 +0400

    Add files via upload

--- chain.c
+++ chain.c
@@ -1,39 +1,33 @@
 #include "TTree.h"
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
-#include "TMath.h"
-#include "TRandom.h"
 #include "TChain.h"
+#include "TH1.h"
+
 
 using std::cout;
 using std::endl;
 
 void chain()
 {
+	TChain chain("T");
 
-TFile ft1("tree1.root","OPEN");
-TFile ft2("tree2.root","OPEN");
-TFile ft3("tree3.root","OPEN");
-TFile all("tree_all.root","RECREATE");
-TTree tall("T1","Tree all");
-
-TChain ch1("ft1");
-TChain ch2("ft2");
-TChain ch3("ft3");
-TChain ch("tall");
-
-ch.AddFriend("ft1");
-ch.AddFriend("ft2");
-ch.AddFriend("ft3");
-ch.Draw("var:T.A:T.B");
-// tall.AddFriend("ft1","tree1.root");
-// tall.AddFriend("ft2","tree2.root");
-// tall.AddFriend("ft3","tree3.root");
-// tall.Draw();
-// tall.Write();
+// TFile ft1 = new TFile("tree1.root","OPEN");
+// TFile ft2("tree2.root","OPEN");
+// TFile ft3("tree3.root","OPEN");
+// TFile all("tree_all.root","RECREATE");
+// TTree tall("T1","Tree all");
 
 
+chain.Add("tree1.root");
+chain.Add("tree2.root");
+chain.Add("tree3.root");
+
+chain.Draw("A");
+TFile* file = TFile::Open("output.root","RECREATE");
+chain.CloneTree(-1,"fast");
+file->Write();
+delete file;
 
 	
 }
