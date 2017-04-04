56b1fe90d195f2587cc5581709aeb5cb15220b60
buzinowski <s.g.buzin@gmail.com>
2017-04-04 21:01:53 +0400

    Add files via upload

--- chain.c
+++ chain.c
@@ -11,23 +11,35 @@ using std::endl;
 void chain()
 {
 	TChain chain("T");
+	TH1F* hist = new TH1F("Histogramm",";x",100,0,100);
+	TH1F* hist1 = new TH1F("Histogramm1",";x",100,0,100);
+	chain.Add("tree1.root");
+	chain.Add("tree2.root");
+	chain.Add("tree3.root");
 
-// TFile ft1 = new TFile("tree1.root","OPEN");
-// TFile ft2("tree2.root","OPEN");
-// TFile ft3("tree3.root","OPEN");
-// TFile all("tree_all.root","RECREATE");
-// TTree tall("T1","Tree all");
+	Float_t a,b;
 
+	chain.SetBranchAddress("A", &a);
+	chain.SetBranchAddress("B", &b);
 
-chain.Add("tree1.root");
-chain.Add("tree2.root");
-chain.Add("tree3.root");
+	int nEntries = chain.GetEntries();
 
-chain.Draw("A");
-TFile* file = TFile::Open("output.root","RECREATE");
-chain.CloneTree(-1,"fast");
-file->Write();
-delete file;
+	for (int i = 0; i < nEntries; i++)
+	{
+		chain.GetEntry(i);
+//		cout << a << endl;
+		hist->Fill(a);
+		hist1->Fill(b);
+	}
+	TCanvas* c1 = new TCanvas("c1","A");
+	hist->Draw();
+	TCanvas* c2 = new TCanvas("c2","B");
+	hist1->Draw();
 
-	
+	//chain.Draw("A");
+	TFile* file = TFile::Open("output.root","RECREATE");
+	chain.CloneTree(-1,"fast");
+	file->Write();
+	delete file;
+
 }
